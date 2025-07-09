81dfb0fc0c2a63b16184d979334b8a072d1e7a11
socrateNz <sssnzogning0@gmail.com>
2025-07-09 18:06:30 +0100

    api link

--- loumo-ui/providers/axios.ts
+++ loumo-ui/providers/axios.ts
@@ -3,7 +3,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 
 const baseURL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api";
+  process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const api = axios.create({
   baseURL,
